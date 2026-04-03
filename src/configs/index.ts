83ee53d1e6e3112c0f52ffbbd7cacdb51cabe055
chirag389817 <chirag389817@gmail.com>
2026-04-03 19:09:22 +0530

    made it static html + css only

--- src/configs/index.ts
+++
@@ -1,6 +0,0 @@
-export const PORT = Number(process.env.PORT ?? 3000);
-
-export type TNodeEnv = "development" | "production";
-export const NODE_ENV = (process.env.NODE_ENV ?? "development") as TNodeEnv;
-
-export const CWD = process.cwd();
